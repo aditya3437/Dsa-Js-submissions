ce925fe99ceb3c9ff513a22c5fdcb6e9954d3e60
Aditya kumar <115103487+aditya3437@users.noreply.github.com>
2026-04-20 23:12:53 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length){
+            return false;
+        }
+        let obj1={};
+        let obj2={};
+
+        for(let i=0;i<s.length;i++){
+            obj1[s[i]]=(obj1[s[i]] || 0) + 1;
+        }
+        for(let i=0;i<t.length;i++){
+            obj2[t[i]]=(obj2[t[i]] || 0) + 1;
+        }
+
+        for(let key in obj1){
+            if(obj1[key] !== obj2[key]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
